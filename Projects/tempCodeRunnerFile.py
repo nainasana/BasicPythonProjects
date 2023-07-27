6ef6a5a1f76493a420065e44799e2822bc86f454
NainaSana <nainasanask1987@gmail.com>
2023-07-27 10:52:03 +0530

    Update tempCodeRunnerFile.py

--- Projects/tempCodeRunnerFile.py
+++ Projects/tempCodeRunnerFile.py
@@ -1,2 +1,12 @@
- 34
-print(_a)
+#Temperature converter
+#1 degree celsius = 33.8 degree farenhiet
+
+#convert celsius into farenhiet using python
+celsius = int(input("Enter the temperature: "))
+farenhiet = (celsius * 1.8)+32
+print(celsius, "degree celsius is equal to", farenhiet, "degree farenhiet")
+
+#convert farenhiet intocelsius using python
+farenhiet = int(input("Enter the temperature: "))
+celsius = (farenhiet - 32)/1.8
+print(farenhiet, "degree celsius is equal to", celsius, "degree farenhiet")
