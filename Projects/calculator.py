9e7c1a45f01a9eaf26b4680b9a14d346de202a92
NainaSana <nainasanask1987@gmail.com>
2023-07-27 10:44:11 +0530

    Update calculator.py

--- Projects/calculator.py
+++ Projects/calculator.py
@@ -1,77 +1,15 @@
-# # a = int(input("Enter first number: "))
-# # b = int(input("Enter second number: "))
-# # operator = input("Enter the operator: ")
-# # if operator == '+':
-# #     print("The addition of", a, b, "is: ", a+b)
-# # elif operator == '-':
-# #     print("The subration of", a, b, "is: ", a-b)
-# # elif operator == '/':
-# #     print("The division of", a, b, "is: ", a/b)
-# # elif operator == '*':
-# #     print("The multiplication of", a, b, "is: ", a*b)
-# # else:
-# #     print("Invalid operator")
+#Calculator using conditional statements
 
-# # claculator using conditional statements
-
-# first_number = int(input("Enter a first number: "))
-# second_number = int(input("Enter a second number: "))
-# operator = input("Enter an operator: ")
-# if operator == '+':
-#     print(first_number + second_number)
-# elif operator == '-':
-#     print(first_number - second_number)
-# elif operator == '*':
-#     print(first_number * second_number)
-# elif operator == '/':
-#     print(first_number / second_number)
-# else:
-#     print("Invalid operator") 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-#calculator
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 operator = input("Enter the operator: ")
 if operator == '+':
-    print("The addition of", a, "and", b, "is", a + b)
+    print("The addition of", a, b, "is: ", a+b)
 elif operator == '-':
-    print(a - b)
-elif operator == '*':
-    print(a* b)
+    print("The subration of", a, b, "is: ", a-b)
 elif operator == '/':
-    print(a/b) 
+    print("The division of", a, b, "is: ", a/b)
+elif operator == '*':
+    print("The multiplication of", a, b, "is: ", a*b)
 else:
     print("Invalid operator")
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
